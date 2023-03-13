73ff1bc65f55f72788e85df1d2d233998cb9fc4d
angelika <dehainaut.angelique@orange.fr>
2023-03-13 09:54:21 +0100

    ==>form article, ==> article for home, ==> add_article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/article', name: 'app_article')]
-    public function index(): Response
+    #[Route('/', name: 'app_article')]
+    public function index(ArticleRepository $articleRepository): Response
     {
-        return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+        return $this->render('home/index.html.twig', [
+            'articles' => $articleRepository->findAll(),
         ]);
     }
+
+    #[Route('/article/add', name: 'article_add')]
+    public function add(Request $request, EntityManagerInterface $entityManager):Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request); #hydrate the article object
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($article); #consider my new article
+            $entityManager->flush(); #send my article to the database
+        }
+
+        return $this->render('article/add.html.twig', [
+           'article_form' => $form->createView(),
+        ]);
+    }
+
+
 }
