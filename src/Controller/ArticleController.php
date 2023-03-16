429b2535dc22795934c8888acc0c7ee0d723e19a
angelika <dehainaut.angelique@orange.fr>
2023-03-16 12:09:48 +0100

    ==> edit, delete comments ok

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -7,7 +7,6 @@ use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
